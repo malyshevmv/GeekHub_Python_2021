b3d536a5a65bcfa24534a0d880e08148709f6c73
malyshevmv <777malysh777@gmail.com>
2022-01-18 20:45:10 +0200

    reworked

--- HT_15/bot/bot/spiders/vikka.py
+++ HT_15/bot/bot/spiders/vikka.py
@@ -12,7 +12,11 @@ class VikkaSpider(scrapy.Spider):
     def parse(self, response):
         """збираємо урли новин на вибраній сторінці"""
         url_new = response.css('h2.title-cat-post a::attr(href)').getall()
-        yield from response.follow_all(url_new, self.parse_new)
+        for new in url_new:
+            yield from response.follow_all(url_new, self.parse_new)
+        next_page = response.css('div.nav-links a::attr(href)').get()
+        if next_page is not None:  # якщо є урла на наступну сторінку - переходимо і  продовжуємо шукати новини
+            yield from response.follow_all(next_page, self.parse)
 
     def parse_new(self, response):
         """збираємо потрібну інформацію зі сторінки"""
@@ -24,5 +28,6 @@ class VikkaSpider(scrapy.Spider):
             'title': response.css('h1.post-title.-margin-b::text').get().strip(),
             'text_list': response.css('p::text').getall(),
             'tag_list': tags,
+            'url': response.url,
         }
         yield inf
