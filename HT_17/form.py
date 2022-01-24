a1e459075b89a515726a0ae15fbd8c51a7bd6592
malyshevmv <777malysh777@gmail.com>
2022-01-24 21:37:22 +0200

    reworked

--- HT_17/form.py
+++ HT_17/form.py
@@ -15,35 +15,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
 driver = webdriver.Chrome()
 
+url = 'https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link'
+driver.get(url)
+wait = WebDriverWait(driver, 2.5)
 
-url_form = 'https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link'
-driver.get(url_form)
-wait = WebDriverWait(driver, 20)
-
-name_box = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')))
+name_box = wait.until(
+    EC.visibility_of_element_located(
+        (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')))
 button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
-def the_for():
-    try:
-        name_box.click()
-        name_box.clear()
-        name_box.send_keys('Mykola')
-    except Exception as err:
-        the_for()
-try:
-    the_for()
-except Exception as err:
-    the_for()
+name_box.click()
+# name_box.clear()
+name_box.send_keys('Mykola')
+
 scr = wait.until(EC.visibility_of_element_located((By.XPATH, '/html')))
 scr.screenshot('screen_form_01.png')
 button.click()
 
 lst_url = driver.current_url
-
-driver2 = webdriver.Chrome()
-driver2.get(lst_url)
 
 lst_page = wait.until(EC.visibility_of_element_located((By.XPATH, '/html')))
 lst_page.screenshot('screen_form_02.png')
