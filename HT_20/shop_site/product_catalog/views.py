605e5270c09730ceb963e4c48769fb69f3086f80
malyshevmv <777malysh777@gmail.com>
2022-02-09 22:08:29 +0200

    no shopping cart and category page

--- HT_20/shop_site/product_catalog/views.py
+++ HT_20/shop_site/product_catalog/views.py
@@ -4,20 +4,37 @@ from django.views import generic
 from .models import Product, Category
 
 
-def index(request):
-    num_product = Product.objects.all().count()
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
-    return render(request, 'product_catalog/index.html', context={
-        'num_product': num_product,
-        'num_visits': num_visits
-    })
-
-
 class ProductListView(generic.ListView):
     model = Product
 
 
 class ProductDetailView(generic.DetailView):
     model = Product
+
+
+class CategoryDetailView(generic.DetailView):
+    model = Category
+
+
+def index(request, pk=None):
+    num_product = Product.objects.all().count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    product_lst = Product.objects.all()
+    category_lst = Category.objects.all()
+    return render(request, 'product_catalog/index.html', context={
+        'num_product': num_product,
+        'num_visits': num_visits,
+        'lst': product_lst,
+        'lst_cat': category_lst
+    })
+
+
+def cat(request, pk=None):
+    product_lst = Product.objects.all()
+    category_lst = Category.objects.all()
+    return render(request, 'product_catalog/category_detail.html', context={
+        'lst': product_lst,
+        'lst_cat': category_lst
+    })
 
--- HT_20/shop_site/shop_site/urls.py
+++ HT_20/shop_site/shop_site/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     re_path(r'^products/$', views.ProductListView.as_view(), name='products'),
-    re_path(r'^product/(?P<pk>\d+)$', views.ProductDetailView.as_view(), name='product-detail'),
+    re_path(r'^products/product/(?P<pk>\d+)$', views.ProductDetailView.as_view(), name='product-detail'),
+    re_path(r'^product/(?P<pk>\d+)$', views.ProductDetailView.as_view()),
+    re_path(r'^category/(?P<pk>\d+)$', views.cat, name='category_detail'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
